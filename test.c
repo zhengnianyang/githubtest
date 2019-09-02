32f47d14d34769ef3e5f9bd6e566dd5707a28f2c
zhengnianyang <54792212+zhengnianyang@users.noreply.github.com>
2019-09-02 16:49:41 +0800

    Update test.c

--- test.c
+++ test.c
@@ -4,7 +4,6 @@ int main(int argc, const char *argv[])
     printf("hello world!\n");
     printf("hello \n");
     printf("feature \n");
-    printf("---------------editor----------\n");
     printf("---------------master----------\n");
     return 0;
 }
